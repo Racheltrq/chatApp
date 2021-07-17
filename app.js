9d71a7808a5e86aae7bac88ead34f2180d3fe743
Rachel Tao <rt1726@nyu.edu>
2021-07-17 16:34:15 -0400

    init chat app

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const socket = require('socket.io')
+
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+var port = process.env.PORT || 3000;
+
+var roomId = ''
+
+//Render Index page
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+//get user input and redirect to room page
+app.post('/room', (req, res) => {
+    roomname = req.body.roomname;
+    username = req.body.username;
+    res.redirect(`/room?username=${username}&roomname=${roomname}`)
+})
+
+//Render Room page
+app.get('/room', (req, res)=>{
+    res.render('room');
+})
+
+//Start Server
+const server = app.listen(port, () => {
+    console.log(`Server Running on port ${port}`)
+})
+
+const io = socket(server);
+require('./utils/socket')(io);
